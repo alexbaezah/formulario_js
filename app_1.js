9785cc1e1de1a9a01b84bc57eb8044a9112d6235
alexbaezah <alcebaeza@gmail.com>
2020-10-18 18:33:27 -0300

    agregado validadores

--- app_1.js
+++ app_1.js
@@ -9,12 +9,32 @@ window.addEventListener('load', function() {
     var box_dashed = document.querySelector(".dashed");
     box_dashed.style.display = "none";
 
+    
+
     formulario.addEventListener('submit', function(){
         console.log("evento submit capturado") //acá probamos si captura el submit
 
         var nombre = document.querySelector("#nombre").value; 
         var apellidos = document.querySelector("#apellidos").value; 
-        var edad = document.querySelector("#edad").value;
+        var edad = parseInt(document.querySelector("#edad").value);
+        
+        if(nombre.trim() === null || nombre.trim().length === 0){
+            alert("el nombre no es válido");
+            document.querySelector("#error_nombre").innerHTML = "El nombre no es válido";
+            return false;
+        }else {
+            document.querySelector("#error_nombre").style.display = "none"
+        }
+
+        if(apellidos.trim() === null || apellidos.trim().length === 0){
+            alert("el apellido no es válido");
+            return false;
+        }
+
+        if(edad === null || edad <= 0 || isNaN(edad)){
+            alert("el edad no es válida");
+            return false;
+        }
 
         box_dashed.style.display = "block";
 
